e77af214be15b676ff9853a8390bc50769d951bc
NguyenDucDung22110301 <22110301@hcmute.student.edu.vn>
2024-09-12 19:00:41 +0700

    bai tap

---
+++ baiTap-tuan4-22110301/src/main/java/controller/changePassword.java
@@ -0,0 +1,87 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.User;
+import service.UserServiceImpl;
+
+import java.io.IOException;
+
+
+/**
+ * Servlet implementation class changePassword
+ */
+@WebServlet("/changePass")
+public class changePassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public changePassword() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String password = request.getParameter("oldpassword");
+		String repassword = request.getParameter("repassword");
+		String newrepassword = request.getParameter("newrepassword");
+		
+		String username = "";
+		String alertMsg ="";
+		HttpSession ses = request.getSession();
+		Object obj = (Object)ses.getAttribute("account");
+		User user = null;
+		UserServiceImpl service = new UserServiceImpl();
+		if(obj != null) {
+			user = (User)obj;
+			username = user.getUserName() + "";
+		}
+		if(user == null) {
+			alertMsg = "chưa tồn tại khách hàng";
+			request.setAttribute("alert", alertMsg);
+			request.getRequestDispatcher("/ChangePass.jsp").forward(request, response);
+		}
+		if(password.equals(user.getPassWord())){
+			if(password.equals(repassword)) {
+				alertMsg = "Mật khẩu mới không được giống mật khẩu cũ";
+				request.setAttribute("alert", alertMsg);
+				request.getRequestDispatcher("/ChangePass.jsp").forward(request, response);
+			}else {
+				if(repassword.equals(newrepassword)) {
+					System.out.println("thay doi mat khau thanh cong");
+					service.changePassword(username, repassword);
+					request.getRequestDispatcher("/successChangePass.jsp").forward(request, response);
+				}else {
+					alertMsg = "Mật khẩu nhập lại không khớp";
+					request.setAttribute("alert", alertMsg);
+					request.getRequestDispatcher("/ChangePass.jsp").forward(request, response);
+				}
+			}
+			alertMsg = "Mật khẩu hiện tại không khớp với mật khẩu cũ";
+			request.setAttribute("alert", alertMsg);
+			request.getRequestDispatcher("/ChangePass.jsp").forward(request, response);
+		}else {
+			alertMsg = "Mật khẩu hiện tại không khớp với mật khẩu cũ";
+			request.setAttribute("alert", alertMsg);
+			request.getRequestDispatcher("/ChangePass.jsp").forward(request, response);
+		}
+		
+	}
+
+}
---
+++ baiTap-tuan4-22110301/src/main/webapp/successChangePass.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> đã thay đổi mật khẩu thành công</title>
+</head>
+<body>
+	<p> thay doi mat khau thanh cong  <a href = "LonginSuccess.jsp"> nhan vao day </a> de chuyen ve trang chu</p>
+</body>
+</html>
